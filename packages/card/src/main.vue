29a62082097395b4a625c23039893a43796a5cd3
cinyearchan <cinyearchan@gmail.com>
2022-02-28 14:41:23 +0800

    feat: card组件样式

---
+++ packages/card/src/main.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="el-card" :class="shadow ? 'is-' + shadow + '-shadow' : 'is-always-shadow'">
+    <div class="el-card__body" :style="bodyStyle">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ElCard',
+  props: {
+    bodyStyle: {},
+    shadow: {},
+  }
+}
+</script>
