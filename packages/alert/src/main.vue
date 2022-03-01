7d5f21da3cd162e887e2118819972d57632c08e2
cinyearchan <cinyearchan@gmail.com>
2022-03-01 17:22:48 +0800

    update alert

--- packages/alert/src/main.vue
+++ packages/alert/src/main.vue
@@ -1,9 +1,42 @@
 <template>
-  <div class="el-alert"></div>
+  <div
+    class="el-alert"
+    :class="[typeClass]"
+  >
+    <div class="el-alert__content">
+      <span>
+        <slot name="title">{{ title }}</slot>
+      </span>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'ElAlert'
+  name: 'ElAlert',
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    type: {
+      type: String,
+      default: 'info'
+    }
+  },
+  computed: {
+    typeClass() {
+      return `el-alert--${this.type}`;
+    },
+    iconClass() {
+      return `el-alert__icon--${this.type}`;
+    },
+    isBigIcon() {
+      return this.description || this.$slots.default ? 'is-big' : '';
+    },
+    isBoldTitle() {
+      return this.description || this.$slots.default ? 'is-bold' : '';
+    }
+  }
 }
 </script>
